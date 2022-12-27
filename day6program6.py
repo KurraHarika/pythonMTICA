1afea19277b831644cd5e72bf715d8811b112df3
KurraHarika <121171553+KurraHarika@users.noreply.github.com>
2022-12-27 16:34:02 +0530

    Add files via upload

---
+++ day6program6.py
@@ -0,0 +1,9 @@
+s0="sheldon Cooper's apartment is in Pasedena"
+s1='This cheese shop's cheese is all gone"
+s2="""We are
+"The Knights of the Round Table"
+"""
+s3="Toto,I said,\n\"We aren't in Kansas,anymore!\""
+s4='Have you seen the "Final Five"'s picture?'
+s5="Have you seen the 'Final Five''s picture?"
+ 
