f2567840badc036e82d8122d47fa927fe91146b7
KurraHarika <121171553+KurraHarika@users.noreply.github.com>
2022-12-27 16:35:46 +0530

    Add files via upload

---
+++ day6program12.py
@@ -0,0 +1,14 @@
+"""ans=[]
+for i in range(900,1001):
+    if '6' in str(i):
+        ans.append(i)
+
+print(ans)"""
+
+
+
+ans=[]
+for i in range(900,1001):
+    if '6' in str(i):
+        ans.append(i)
+print(ans)
